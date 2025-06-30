d08136e357b689e1a0ef38f4e7cf5133c52d9932
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2025-06-30 19:07:07 +0100

    fix: sequencing of audio element creations
    not very happy with the infinite loop hogging CPU and adding in 250ms
    latency

--- frontend/app.js
+++ frontend/app.js
@@ -3,27 +3,36 @@ const txtinput = document.getElementById("text");
 const btnsubmit = document.getElementById("submit");
 const audioContainer = document.getElementById("audioContainer");
 
-function submit() {
-  const texts = txtinput.value.split("\n").filter(Boolean);
-  createAudio(texts);
-  txtinput.value = "";
-}
+const queue = [];
 
-const createAudio = (txts) => {
-  if (txts.length === 0) {
-    console.log("all done");
+const performEffect = () => {
+  console.log("PERFORM:", Date.now());
+  if (queue.length === 0) {
+    setTimeout(performEffect, 500);
   } else {
-    const [txt, ...tail] = txts;
+    console.log("Stuff in the queue");
+    const txt = queue.shift();
     logentry(txt);
     const text = encodeURI(txt);
     const audio = new Audio(`http://localhost:5000/?text=${text}`);
     audio.setAttribute("controls", "");
     audioContainer.appendChild(audio);
     audio.addEventListener("loadeddata", () => {
-      createAudio(tail);
+      performEffect();
     });
   }
 };
+
+performEffect();
+
+function submit() {
+  console.log("Submitting");
+  const texts = txtinput.value.split("\n").filter(Boolean);
+  texts.forEach((text) => {
+    queue.push(text);
+  });
+  txtinput.value = "";
+}
 
 function logentry(input) {
   const timestamp = new Date().toLocaleTimeString();
