671e2bc4bdfb23c7dff8accd55417c2a7cf120b5
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2025-06-30 13:45:00 +0100

    feat: chunk text and load sequentially
    still WIP, it's possible to end up with interleaved audio elements
    due to race condition

--- frontend/app.js
+++ frontend/app.js
@@ -1,27 +1,42 @@
 const txtlog = document.getElementById("log");
 const txtinput = document.getElementById("text");
 const btnsubmit = document.getElementById("submit");
+const audioContainer = document.getElementById("audioContainer");
 
-function play() {
-  var text = encodeURI(txtinput.value);
-  const audio = new Audio(`http://localhost:5000/?text=${text}`);
-  audio.play();
-  logentry(txtinput.value);
+function submit() {
+  const texts = txtinput.value.split("\n").filter(Boolean);
+  createAudio(texts);
   txtinput.value = "";
 }
 
+const createAudio = (txts) => {
+  if (txts.length === 0) {
+    console.log("all done");
+  } else {
+    const [txt, ...tail] = txts;
+    logentry(txt);
+    const text = encodeURI(txt);
+    const audio = new Audio(`http://localhost:5000/?text=${text}`);
+    audio.setAttribute("controls", "");
+    audioContainer.appendChild(audio);
+    audio.addEventListener("loadeddata", () => {
+      createAudio(tail);
+    });
+  }
+};
+
 function logentry(input) {
   const timestamp = new Date().toLocaleTimeString();
-  txtlog.value += "\n" + timestamp + " : " + input;
+  txtlog.value += "\n" + timestamp + " : " + input + "\n---";
 }
 
 btnsubmit.addEventListener("click", () => {
-  play();
+  submit();
 });
 
 txtinput.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
-    play();
+    submit();
   }
 });
 
